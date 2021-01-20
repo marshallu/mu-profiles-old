a8f1d1e5e4e8d790c7d01543b97561c1dd21c8b0
Christopher McComas <cmccomas@marshall.edu>
2021-01-20 15:31:27 -0500

    wip

--- templates/taxonomy-department.php
+++ templates/taxonomy-department.php
@@ -22,10 +22,11 @@ require get_template_directory() . '/template-parts/hero/no-hero.php';
 				?>
 			</header><!-- .page-header -->
 				<?php
+
 				$the_term = get_queried_object();
 
 				$dept_listing = get_field( 'department_listing_display', $the_term );
-
+				echo '<!-- ' . $dept_listing . ' -->';
 				if ( 'row' === $dept_listing ) {
 					include plugin_dir_path( __FILE__ ) . '/row.php';
 				} elseif ( 'enhanced' === $dept_listing ) {
