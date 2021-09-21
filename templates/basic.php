8ff0e0c547749ca529b2d3616929953ee924c1a8
Christopher McComas <cmccomas@marshall.edu>
2021-09-21 13:19:14 -0400

    change width of box based on template

--- templates/basic.php
+++ templates/basic.php
@@ -7,6 +7,11 @@
  * @package MU Profiles
  */
 
+if ( is_page_template( array( 'page-full-width.php', 'page-full-width-hero.php', 'page-secondary-nav.php', 'page-secondary-classic.php', 'page-experience.php' ) ) ) {
+	$width = ' lg:w-1/3 ';
+} else {
+	$width = ' lg:w-1/2 ';
+}
 ?>
 <div class="">
 	<!-- <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2> -->
@@ -16,7 +21,7 @@
 			the_post();
 			$image = get_field( 'employee_headshot' );
 			?>
-			<div class="w-full lg:w-1/2 lg:px-6 mb-8">
+			<div class="w-full <?php echo esc_attr( $width ); ?> lg:px-6 mb-8">
 				<div class="flex flex-wrap flex-row lg:-mx-2">
 					<div class="w-full lg:w-1/4 lg:px-2">
 						<img class="object-cover shadow-sm rounded-lg" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
