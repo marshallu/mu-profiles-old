bda12903c30033a42232beed50dc85d02723e507
Christopher McComas <cmccomas@marshall.edu>
2021-09-21 13:17:36 -0400

    wip

--- templates/basic.php
+++ templates/basic.php
@@ -8,34 +8,30 @@
  */
 
 ?>
-<div class="bg-white">
-	<div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-		<div class="">
-			<!-- <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2> -->
-			<div class="flex flex-wrap lg:-mx-6">
-				<?php
-				while ( have_posts() ) :
-					the_post();
-					$image = get_field( 'employee_headshot' );
-					?>
-					<div class="w-full lg:w-1/2 lg:px-6 mb-8">
-						<div class="flex flex-wrap flex-row lg:-mx-2">
-							<div class="w-full lg:w-1/4 lg:px-2">
-								<img class="object-cover shadow-sm rounded-lg" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
-							</div>
-							<div class="w-full lg:w-3/4 lg:px-2 mt-4 lg:mt-0">
-								<div class="text-lg font-semibold space-y-1">
-									<div><?php echo esc_attr( get_the_title() ); ?></div>
-									<p class="text-gray-500"><?php the_field( 'employee_position' ); ?></p>
-								</div>
-								<div class="text-lg mt-1">
-									<p class="text-gray-500"><?php the_field( 'employee_biography' ); ?></p>
-								</div>
-							</div>
+<div class="">
+	<!-- <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Meet our leadership</h2> -->
+	<div class="flex flex-wrap lg:-mx-6">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			$image = get_field( 'employee_headshot' );
+			?>
+			<div class="w-full lg:w-1/2 lg:px-6 mb-8">
+				<div class="flex flex-wrap flex-row lg:-mx-2">
+					<div class="w-full lg:w-1/4 lg:px-2">
+						<img class="object-cover shadow-sm rounded-lg" src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+					</div>
+					<div class="w-full lg:w-3/4 lg:px-2 mt-4 lg:mt-0">
+						<div class="text-lg font-semibold space-y-1">
+							<div><?php echo esc_attr( get_the_title() ); ?></div>
+							<p class="text-gray-500"><?php the_field( 'employee_position' ); ?></p>
+						</div>
+						<div class="text-lg mt-1">
+							<p class="text-gray-500"><?php the_field( 'employee_biography' ); ?></p>
 						</div>
 					</div>
-				<?php endwhile; ?>
+				</div>
 			</div>
-		</div>
+		<?php endwhile; ?>
 	</div>
 </div>
